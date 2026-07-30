d8867a28bb16c200897c9f999d6e32e24479f4e6
VMurtis <v.murtis@gmail.com>
2026-07-30 12:36:19 -0300

    separa app em models, views e config

---
+++ config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'flask'
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = '',
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
