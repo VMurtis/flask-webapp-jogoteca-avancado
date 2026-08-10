64ea26d42ea3803420078beb0bf41050629b1d23
VMurtis <v.murtis@gmail.com>
2026-08-10 16:27:35 -0300

    corrige atualizacao e exclusao de capas

--- views_game.py
+++ views_game.py
@@ -73,7 +73,7 @@ def atualizar():
         arquivo = request.files['arquivo']
         upload_path = app.config['UPLOAD_PATH']
         timestamp = time.time()
-        deleta_arquivo(id)
+        deleta_arquivo(jogo.id)
         arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
 
     return redirect(url_for('index'))
